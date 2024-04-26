40c431789e977e095f4a4e717970ef72c439dff0
Петровски Никола <nikola.petrovski.2@students.finki.ukim.mk>
2024-04-26 17:03:54 +0200

    Inserting and mapping data from api call

--- cron_job.php
+++ cron_job.php
@@ -1,13 +1,11 @@
 <?php
 
-//require_once 'vendor/autoload.php';
-//use JMS\Serializer\SerializerBuilder;
-use Models\RootObject;
+//require('../wp-includes/wp-db.php');
 
     global $wpdb;
 
     $prefix = 'Y7FXuNUTt';
-    $url = 'https://api.worldota.net/api/b2b/v3/hotel/info/?data={"id":"beach_studio_apartment_3_komi","language":"en"}';
+    $url = 'https://api.worldota.net/api/b2b/v3/hotel/info/?data={"id":"lagomandra_hotel_and_spa","language":"en"}';
     $keyId = '7788';
     $apiKey = 'e6a79dc0-c452-48e0-828d-d37614165e39';
 
@@ -26,37 +24,77 @@ use Models\RootObject;
  
     $data2 = json_decode($response, true);
 
-    print_r($data2);
+    $current_date_time = date('Y-m-d H:i:s');
 
-    $res = $wpdb->insert(
-        $prefix . '_posts',
+    $post_content = '';
+    $post_title = $data2['data']['name'];
+    $post_excerpt = $data2['data']['metapolicy_extra_info'];
+    $address = $data2['data']['address'];
+    $star_rating = $data2['data']['star_rating'];
+    $latitude = $data2['data']['latitude'];
+    $longitude = $data2['data']['longitude'];
+
+    foreach ($data2['data']['description_struct'] as $content){
+        foreach ($content['paragraphs'] as $paragraph){
+            $post_content .= $paragraph . '<br><br>';
+        }
+    }
+
+    try {
+        $wpdb->insert(
+            $prefix . '_posts',
+            array(
+                'post_author' => 14,
+                'post_date' => $current_date_time,
+                'post_date_gmt' => $current_date_time,
+                'post_content' => $post_content,
+                'post_title' => $post_title,
+                'post_excerpt' => $post_excerpt,
+                'post_status' => 'draft',
+                'comment_status' => 'open',
+                'ping_status' => 'open',
+                'post_password' => '',
+                'post_name' => $post_title,
+                'to_ping' => '',
+                'pinged' => '',
+                'post_modified' => $current_date_time,
+                'post_modified_gmt' => $current_date_time,
+                'post_content_filtered' => '',
+                'post_parent' => 0,
+                'guid' => '',
+                'menu_order' => 0,
+                'post_type' => 'st_hotel',
+                'post_mime_type' => '',
+                'comment_count' => 0
+            )
+        );
+        echo 'Data inserted successfully';
+    } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    }
+
+    $post_id = $wpdb->get_results("SELECT ID FROM " . $prefix . "_posts WHERE post_title = '" . $post_title . "'");
+
+    $wpdb->insert(
+        $prefix . '_postmeta',
         array(
-            'post_author' => 6,
-            'post_date' => '2024-04-26 14:01:44',
-            'post_date_gmt' => '2024-04-26 14:01:44',
-            'post_content' => 'TEST TITLE',
-            'post_title' => 'TEST TITLE',
-            'post_excerpt' => 'TEST TITLE',
-            'post_status' => 'draft',
-            'comment_status' => 'open',
-            'ping_status' => 'open',
-            'post_password' => '',
-            'post_name' => '',
-            'to_ping' => '',
-            'pinged' => '',
-            'post_modified' => '2024-04-26 14:01:44',
-            'post_modified_gmt' => '2024-04-26 14:01:44',
-            'post_content_filtered' => '',
-            'post_parent' => 0,
-            'guid' => '',
-            'menu_order' => 0,
-            'post_type' => 'post',
-            'post_mime_type' => '',
-            'comment_count' => 0
+            'post_id' => $post_id,
+            'multi_location' => '_14848_,_15095_',
+            'id_location' => '',
+            'address' => $address,
+            'allow_full_day' => 'on',
+            'rate_review' => $star_rating,
+            'hotel_star' => 0,
+            'price_avg' => 0,
+            'min_price' => 0,
+            'hotel_booking_period' => 0,
+            'map_lat' => $latitude,
+            'map_lng' => $longitude,
+            'is_sale_schedule' => NULL,
+            'post_origin' => NULL,
+            'is_featured' => 'off'
         )
     );
-    
-    print_r($res);
 
     curl_close($ch);
 
